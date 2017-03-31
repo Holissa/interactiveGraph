d537fd36e6c76a11ea75fa60db72030430b2c2c9
holissa <holissa@hotmail.com>
2017-04-01 02:05:41 +0300

    added true style for graph, started work with indicator line

--- app/app.component.js
+++ app/app.component.js
@@ -19,19 +19,20 @@ var Item = (function () {
 exports.Item = Item;
 var AppComponent = (function () {
     function AppComponent() {
+        this.par = 70;
         this.items = [
             { name: "Январь", value: 113 },
-            { name: "Февраль", value: 259 },
-            { name: "Март", value: 405 },
-            { name: "Апрель", value: 551 },
-            { name: "Май", value: 697 },
-            { name: "Июнь", value: 843 },
-            { name: "Июль", value: 989 },
-            { name: "Август", value: 1135 },
-            { name: "Сентябрь", value: 1281 },
-            { name: "Октябрь", value: 1427 },
-            { name: "Ноябрь", value: 1573 },
-            { name: "Декабрь", value: 1719 }
+            { name: "Февраль", value: 213 },
+            { name: "Март", value: 313 },
+            { name: "Апрель", value: 413 },
+            { name: "Май", value: 513 },
+            { name: "Июнь", value: 613 },
+            { name: "Июль", value: 713 },
+            { name: "Август", value: 813 },
+            { name: "Сентябрь", value: 913 },
+            { name: "Октябрь", value: 1013 },
+            { name: "Ноябрь", value: 1113 },
+            { name: "Декабрь", value: 1213 }
         ];
         this.counter = this.changedItem();
     }
@@ -40,6 +41,12 @@ var AppComponent = (function () {
         var val = val.toString();
         console.log(typeof val);
     };
+    ;
+    AppComponent.prototype.getPx = function (event) {
+        console.log(event);
+        this.par = event;
+    };
+    ;
     AppComponent.prototype.changedItem = function () {
         var count = 113;
         return function () {
@@ -50,7 +57,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n        <svg class=\"graph\" version=\"1.1\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns=\"http://www.w3.org/2000/svg\">\n\n            <g class=\"grid y-grid\" id=\"yGrid\">\n                <line x1=\"86\" x2=\"95%\" y1=\"126\" y2=\"126\"></line>\n                <line x1=\"86\" x2=\"95%\" y1=\"185\" y2=\"185\"></line>\n                <line x1=\"86\" x2=\"95%\" y1=\"243\" y2=\"243\"></line>\n                <line x1=\"86\" x2=\"95%\" y1=\"301\" y2=\"301\"></line>\n                <line x1=\"86\" x2=\"95%\" y1=\"360\" y2=\"360\"></line>\n            </g>\n\n\n            <polyline\n                    fill=\"none\"\n                    stroke=\"#0074d9\"\n                    stroke-width=\"2\"\n                    points=\"\n       00,120\n       20,60\n       40,80\n       60,20\n       80,80\n       100,80\n       120,60\n       140,100\n       160,90\n       180,80\n       200, 110\n       220, 10\n       240, 70\n       260, 100\n       280, 100\n       300, 40\n       320, 0\n       340, 100\n       360, 100\n       380, 120\n       400, 60\n       420, 70\n       440, 80\n     \"\n            />\n\n            <use class=\"grid double\" xlink:href=\"#xGrid\" style=\"\"></use>\n            <use class=\"grid double\" xlink:href=\"#yGrid\" style=\"\"></use>\n\n            <g class=\"first_set points\" data-setname=\"Our first data set\">\n                <circle cx=\"113\" cy=\"192\" data-value=\"7.2\" r=\"5\"></circle>\n                <circle cx=\"259\" cy=\"171\" data-value=\"8.1\" r=\"5\"></circle>\n                <circle cx=\"405\" cy=\"179\" data-value=\"7.7\" r=\"5\"></circle>\n                <circle cx=\"551\" cy=\"200\" data-value=\"6.8\" r=\"5\"></circle>\n                <circle cx=\"697\" cy=\"204\" data-value=\"6.7\" r=\"5\"></circle>\n            </g>\n\n            <g class=\"labels x-labels\">\n                <!--<text x=\"113\" y=\"400\" *ngFor=\"let item of items\">{{item.name}}</text>-->\n                <text [attr.x]=\"item.value\" [attr.y]=\"400\" *ngFor=\"let item of items; let i=index; let last = last;\"(click)=\"getValue(i)\">\n                    {{item.name}}\n                </text>\n                <!--<text x=\"259\" y=\"400\">\u0424\u0435\u0432\u0440\u0430\u043B\u044C</text>-->\n                <!--<text x=\"405\" y=\"400\">\u041C\u0430\u0440\u0442</text>-->\n                <!--<text x=\"551\" y=\"400\">\u0410\u043F\u0440\u0435\u043B\u044C</text>-->\n                <!--<text x=\"697\" y=\"400\">\u041C\u0430\u0439</text>-->\n\n                <!--<text x=\"843\" y=\"400\">\u0418\u044E\u043D\u044C</text>-->\n                <!--<text x=\"989\" y=\"400\">\u0418\u044E\u043B\u044C</text>-->\n                <!--<text x=\"1135\" y=\"400\">\u0410\u0432\u0433\u0443\u0441\u0442</text>-->\n                <!--<text x=\"1281\" y=\"400\">\u0421\u0435\u043D\u0442\u0430\u0431\u0440\u044C</text>-->\n                <!--<text x=\"1427\" y=\"400\">\u041E\u043A\u0442\u044F\u0431\u0440\u044C</text>-->\n\n                <!--<text x=\"1573\" y=\"400\">\u041D\u043E\u044F\u0431\u0440\u044C</text>-->\n                <!--<text x=\"1719\" y=\"400\">\u0414\u0435\u043A\u0430\u0431\u0440\u044C</text>-->\n\n                <text x=\"105\" y=\"425\" font-size=\"16px\">2015</text>\n            </g>\n\n\n            <g class=\"labels y-labels\">\n                <text x=\"70\" y=\"120\">80</text>\n                <text x=\"70\" y=\"180\">60</text>\n                <text x=\"70\" y=\"240\">40</text>\n                <text x=\"70\" y=\"300\">20</text>\n                <text x=\"70\" y=\"365\">0</text>\n            </g>\n        </svg>\n\n    ",
+            template: "\n        <svg class=\"graph\" version=\"1.1\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns=\"http://www.w3.org/2000/svg\"\n             (mousemove)=\"getPx($event.clientX)\">\n            \n            <g class=\"indicator\">\n                <line [attr.x1]=\"par\" y1=\"110\" [attr.x2]=\"par\" y2=\"380\"></line>\n            </g>\n            \n            <g class=\"grid y-grid\" id=\"yGrid\">\n                <line x1=\"86\" x2=\"95%\" y1=\"126\" y2=\"126\"></line>\n                <line x1=\"86\" x2=\"95%\" y1=\"185\" y2=\"185\"></line>\n                <line x1=\"86\" x2=\"95%\" y1=\"243\" y2=\"243\"></line>\n                <line x1=\"86\" x2=\"95%\" y1=\"301\" y2=\"301\"></line>\n                <line x1=\"86\" x2=\"95%\" y1=\"360\" y2=\"360\"></line>\n            </g>\n\n            \n            <polyline\n                    fill=\"none\"\n                    stroke=\"#0074d9\"\n                    stroke-width=\"2\"\n                    points=\"\n                            110,130       \n                            120,135      \n                            160, 140  \n                            170,125   \n                            175,160     \n                            185,165  \n                            195,175  \n                            205,165 \n                            260, 160 \n                            290, 150 \n                            315,180 \n                            320,155\n                            395,175 \n                            470,180 \n                            500,170   \n                            510,180  \n                            520,165 \n                            540,165 \n                            550,185 \n                            560,240  \n                            580,240  \n                            580,245  \n                            625,245\n                            650,300   \n                            680,300  \n                            690,305   \n                            750,240   \n                            760,260   \n                            840,265  \n                            860,250   \n                            875,240    \n                            895,255 \n                            895,265  \n                            910,250  \n                            930,200  \n                            945,185\n                       \n                            960,205 \n                            980, 230  \n                            1020,230   \n                            1050,255  \n                            1060,275 \n                            1080,275  \n                            1100,255 \n                            1155,270 \n                            1185,210 \n                            1240,170\n     \"\n            />\n\n            <g class=\"labels x-labels\">\n                <text [attr.x]=\"item.value\" [attr.y]=\"400\" *ngFor=\"let item of items; let i=index; let last = last;\"\n                      (click)=\"getValue(i)\">\n                    {{item.name}}\n                </text>\n                <text x=\"105\" y=\"425\" font-size=\"16px\">2015</text>\n            </g>\n\n            <g class=\"labels y-labels\">\n                <text x=\"70\" y=\"120\">80</text>\n                <text x=\"70\" y=\"180\">60</text>\n                <text x=\"70\" y=\"240\">40</text>\n                <text x=\"70\" y=\"300\">20</text>\n                <text x=\"70\" y=\"365\">0</text>\n            </g>\n        </svg>\n\n    ",
             styleUrls: ['app/app.component.css']
         }), 
         __metadata('design:paramtypes', [])
--- app/app.component.ts
+++ app/app.component.ts
@@ -16,8 +16,13 @@ export class Item {
 @Component({
     selector: 'my-app',
     template: `
-        <svg class="graph" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
-
+        <svg class="graph" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"
+             (mousemove)="getPx($event.clientX)">
+            
+            <g class="indicator">
+                <line [attr.x1]="par" y1="110" [attr.x2]="par" y2="380"></line>
+            </g>
+            
             <g class="grid y-grid" id="yGrid">
                 <line x1="86" x2="95%" y1="126" y2="126"></line>
                 <line x1="86" x2="95%" y1="185" y2="185"></line>
@@ -26,71 +31,69 @@ export class Item {
                 <line x1="86" x2="95%" y1="360" y2="360"></line>
             </g>
 
-
+            
             <polyline
                     fill="none"
                     stroke="#0074d9"
                     stroke-width="2"
                     points="
-       00,120
-       20,60
-       40,80
-       60,20
-       80,80
-       100,80
-       120,60
-       140,100
-       160,90
-       180,80
-       200, 110
-       220, 10
-       240, 70
-       260, 100
-       280, 100
-       300, 40
-       320, 0
-       340, 100
-       360, 100
-       380, 120
-       400, 60
-       420, 70
-       440, 80
+                            110,130       
+                            120,135      
+                            160, 140  
+                            170,125   
+                            175,160     
+                            185,165  
+                            195,175  
+                            205,165 
+                            260, 160 
+                            290, 150 
+                            315,180 
+                            320,155
+                            395,175 
+                            470,180 
+                            500,170   
+                            510,180  
+                            520,165 
+                            540,165 
+                            550,185 
+                            560,240  
+                            580,240  
+                            580,245  
+                            625,245
+                            650,300   
+                            680,300  
+                            690,305   
+                            750,240   
+                            760,260   
+                            840,265  
+                            860,250   
+                            875,240    
+                            895,255 
+                            895,265  
+                            910,250  
+                            930,200  
+                            945,185
+                       
+                            960,205 
+                            980, 230  
+                            1020,230   
+                            1050,255  
+                            1060,275 
+                            1080,275  
+                            1100,255 
+                            1155,270 
+                            1185,210 
+                            1240,170
      "
             />
 
-            <use class="grid double" xlink:href="#xGrid" style=""></use>
-            <use class="grid double" xlink:href="#yGrid" style=""></use>
-
-            <g class="first_set points" data-setname="Our first data set">
-                <circle cx="113" cy="192" data-value="7.2" r="5"></circle>
-                <circle cx="259" cy="171" data-value="8.1" r="5"></circle>
-                <circle cx="405" cy="179" data-value="7.7" r="5"></circle>
-                <circle cx="551" cy="200" data-value="6.8" r="5"></circle>
-                <circle cx="697" cy="204" data-value="6.7" r="5"></circle>
-            </g>
-
             <g class="labels x-labels">
-                <!--<text x="113" y="400" *ngFor="let item of items">{{item.name}}</text>-->
-                <text [attr.x]="item.value" [attr.y]="400" *ngFor="let item of items; let i=index; let last = last;"(click)="getValue(i)">
+                <text [attr.x]="item.value" [attr.y]="400" *ngFor="let item of items; let i=index; let last = last;"
+                      (click)="getValue(i)">
                     {{item.name}}
                 </text>
-                <!--<text x="259" y="400">Февраль</text>-->
-                <!--<text x="405" y="400">Март</text>-->
-                <!--<text x="551" y="400">Апрель</text>-->
-                <!--<text x="697" y="400">Май</text>-->
-
-                <!--<text x="843" y="400">Июнь</text>-->
-                <!--<text x="989" y="400">Июль</text>-->
-                <!--<text x="1135" y="400">Август</text>-->
-                <!--<text x="1281" y="400">Сентабрь</text>-->
-                <!--<text x="1427" y="400">Октябрь</text>-->
-
-                <!--<text x="1573" y="400">Ноябрь</text>-->
-                <!--<text x="1719" y="400">Декабрь</text>-->
-
                 <text x="105" y="425" font-size="16px">2015</text>
             </g>
-
 
             <g class="labels y-labels">
                 <text x="70" y="120">80</text>
@@ -106,20 +109,23 @@ export class Item {
 })
 
 export class AppComponent {
+    par = 70;
+
     items: Item[] =
         [
             {name: "Январь", value: 113},
-            {name: "Февраль", value: 259},
-            {name: "Март", value: 405},
-            {name: "Апрель", value: 551},
-            {name: "Май", value: 697},
-            {name: "Июнь", value: 843},
-            {name: "Июль", value: 989},
-            {name: "Август", value: 1135},
-            {name: "Сентябрь", value: 1281},
-            {name: "Октябрь", value: 1427},
-            {name: "Ноябрь", value: 1573},
-            {name: "Декабрь", value: 1719}
+            {name: "Февраль", value: 213},
+            {name: "Март", value: 313},
+            {name: "Апрель", value: 413},
+            {name: "Май", value: 513},
+            {name: "Июнь", value: 613},
+            {name: "Июль", value: 713},
+            {name: "Август", value: 813},
+            {name: "Сентябрь", value: 913},
+            {name: "Октябрь", value: 1013},
+            {name: "Ноябрь", value: 1113},
+            {name: "Декабрь", value: 1213}
+
         ];
 
 
@@ -128,7 +134,13 @@ export class AppComponent {
         var val = val.toString();
         console.log(typeof val);
 
-    }
+    };
+
+    getPx(event): void {
+        console.log(event);
+        this.par = event;
+
+    };
 
     changedItem() {
         var count = 113;
