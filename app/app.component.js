de40b51fe66e38055e3b8434e65411726d33717f
holissa <holissa@hotmail.com>
2017-04-02 01:48:33 +0300

    added functional for indicator line

--- app/app.component.js
+++ app/app.component.js
@@ -20,6 +20,13 @@ exports.Item = Item;
 var AppComponent = (function () {
     function AppComponent() {
         this.par = 70;
+        this.x = 0;
+        this.indKey = false;
+        this.monthName = 1;
+        this.pointCorX = 110;
+        this.d = document;
+        this.pointCorY = 130;
+        this.numDay = 1;
         this.items = [
             { name: "Январь", value: 113 },
             { name: "Февраль", value: 213 },
@@ -34,6 +41,7 @@ var AppComponent = (function () {
             { name: "Ноябрь", value: 1113 },
             { name: "Декабрь", value: 1213 }
         ];
+        this.months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
         this.counter = this.changedItem();
     }
     AppComponent.prototype.getValue = function (i) {
@@ -43,8 +51,26 @@ var AppComponent = (function () {
     };
     ;
     AppComponent.prototype.getPx = function (event) {
-        console.log(event);
         this.par = event;
+        this.monthName = Math.floor(this.par / 98);
+        var polyline = this.d.getElementById('poly'), points = polyline.getAttribute('points'), reg = /\d+/g, coordinatAr = points.match(reg); //сформируем массив координат графика
+        for (var i = 0, max = coordinatAr.length; i < max; i++) {
+            if (i % 2 == 0) {
+                if (this.par == Number(coordinatAr[i])) {
+                    this.pointCorX = this.par;
+                    this.pointCorY = Number(coordinatAr[i + 1]);
+                    return;
+                }
+            }
+        }
+    };
+    ;
+    AppComponent.prototype.getW = function (event) {
+        // event.currentTarget.getBBox().width;
+        var lineLength = event.currentTarget.getBBox().width; //длина линии
+        // var monthLength = Math.floor(lineLength / 12); //найдем сколько пикселей приходиться на каждый месяц (98px)
+        var monthLength = Math.floor(lineLength / 365); //найдем сколько пикселей приходиться на каждый день (3px)
+        console.log(monthLength);
     };
     ;
     AppComponent.prototype.changedItem = function () {
@@ -57,7 +83,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n        <svg class=\"graph\" version=\"1.1\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns=\"http://www.w3.org/2000/svg\"\n             (mousemove)=\"getPx($event.clientX)\">\n            \n            <g class=\"indicator\">\n                <line [attr.x1]=\"par\" y1=\"110\" [attr.x2]=\"par\" y2=\"380\"></line>\n            </g>\n            \n            <g class=\"grid y-grid\" id=\"yGrid\">\n                <line x1=\"86\" x2=\"95%\" y1=\"126\" y2=\"126\"></line>\n                <line x1=\"86\" x2=\"95%\" y1=\"185\" y2=\"185\"></line>\n                <line x1=\"86\" x2=\"95%\" y1=\"243\" y2=\"243\"></line>\n                <line x1=\"86\" x2=\"95%\" y1=\"301\" y2=\"301\"></line>\n                <line x1=\"86\" x2=\"95%\" y1=\"360\" y2=\"360\"></line>\n            </g>\n\n            \n            <polyline\n                    fill=\"none\"\n                    stroke=\"#0074d9\"\n                    stroke-width=\"2\"\n                    points=\"\n                            110,130       \n                            120,135      \n                            160, 140  \n                            170,125   \n                            175,160     \n                            185,165  \n                            195,175  \n                            205,165 \n                            260, 160 \n                            290, 150 \n                            315,180 \n                            320,155\n                            395,175 \n                            470,180 \n                            500,170   \n                            510,180  \n                            520,165 \n                            540,165 \n                            550,185 \n                            560,240  \n                            580,240  \n                            580,245  \n                            625,245\n                            650,300   \n                            680,300  \n                            690,305   \n                            750,240   \n                            760,260   \n                            840,265  \n                            860,250   \n                            875,240    \n                            895,255 \n                            895,265  \n                            910,250  \n                            930,200  \n                            945,185\n                       \n                            960,205 \n                            980, 230  \n                            1020,230   \n                            1050,255  \n                            1060,275 \n                            1080,275  \n                            1100,255 \n                            1155,270 \n                            1185,210 \n                            1240,170\n     \"\n            />\n\n            <g class=\"labels x-labels\">\n                <text [attr.x]=\"item.value\" [attr.y]=\"400\" *ngFor=\"let item of items; let i=index; let last = last;\"\n                      (click)=\"getValue(i)\">\n                    {{item.name}}\n                </text>\n                <text x=\"105\" y=\"425\" font-size=\"16px\">2015</text>\n            </g>\n\n            <g class=\"labels y-labels\">\n                <text x=\"70\" y=\"120\">80</text>\n                <text x=\"70\" y=\"180\">60</text>\n                <text x=\"70\" y=\"240\">40</text>\n                <text x=\"70\" y=\"300\">20</text>\n                <text x=\"70\" y=\"365\">0</text>\n            </g>\n        </svg>\n\n    ",
+            template: "\n        <svg class=\"graph\" version=\"1.1\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns=\"http://www.w3.org/2000/svg\" (mousemove)=\"getPx($event.clientX)\">\n\n            \n            <!--<g class=\"indicator\" [ngStyle]=\"{'visibility': (indKey == true) ? 'visible' : 'hidden'}\">-->\n\n            <!--<g class=\"grid y-grid\" id=\"yGrid\" (mousemove)=\"getW($event)\">-->\n            <g class=\"grid y-grid\" id=\"yGrid\">\n                <line x1=\"86\" x2=\"95%\" y1=\"126\" y2=\"126\"></line>\n                <line x1=\"86\" x2=\"95%\" y1=\"185\" y2=\"185\"></line>\n                <line x1=\"86\" x2=\"95%\" y1=\"243\" y2=\"243\"></line>\n                <line x1=\"86\" x2=\"95%\" y1=\"301\" y2=\"301\"></line>\n                <line x1=\"86\" x2=\"95%\" y1=\"360\" y2=\"360\"></line>\n            </g>\n\n            <!-------------------------------------------------------- GRAPH -------------------------------------------------------->\n            <polyline id=\"poly\"\n                      fill=\"none\"\n                      stroke=\"#0074d9\"\n                      stroke-width=\"2\"\n                      points=\"\n                            110,130       \n                            120,135      \n                            160,140  \n                            170,125   \n                            175,160     \n                            185,165  \n                            195,175  \n                            205,165 \n                            260,160 \n                            290,150 \n                            315,180 \n                            320,155\n                            395,175 \n                            470,180 \n                            500,170   \n                            510,180  \n                            520,165 \n                            540,165 \n                            550,185 \n                            560,240  \n                            580,240  \n                            580,245  \n                            625,245\n                            650,300   \n                            680,300  \n                            690,305   \n                            750,240   \n                            760,260   \n                            840,265  \n                            860,250   \n                            875,240    \n                            895,255 \n                            895,265  \n                            910,250  \n                            930,200  \n                            945,185\n                            960,205 \n                            980,230  \n                            1020,230   \n                            1050,255  \n                            1060,275 \n                            1080,275  \n                            1100,255 \n                            1155,270 \n                            1185,210 \n                            1240,170\n     \"\n            />\n            <!-------------------------------------------------------- END GRAPH -------------------------------------------------------->\n\n\n            \n            <!-------------------------------------------------------- INDICATOR  -------------------------------------------------------->\n            <circle class=\"shadow\" [attr.cx]=\"pointCorX\" [attr.cy]=\"pointCorY\" r=\"5\" stroke=\"#AAA\" stroke-width=\"4\"\n                    stroke-opacity=\"1\"></circle>\n            \n            <g>\n                <filter id=\"dropShadow\">\n                    <feGaussianBlur in=\"SourceAlpha\" stdDeviation=\"3\"/>\n                    <feOffset dx=\"2\" dy=\"4\"/>\n                    <feMerge>\n                        <feMergeNode/>\n                        <feMergeNode in=\"SourceGraphic\"/>\n                    </feMerge>\n                </filter>\n                <rect [attr.x]=\"par\" y=\"10\" width=\"200\" height=\"100\" fill=\"white\" filter=\"url(#dropShadow)\"/>\n                <text [attr.x]=\"par+70\" y=\"22%\" alignment-baseline=\"middle\" text-anchor=\"middle\">\n                    {{pointCorX}}{{months[monthName - 1]}} 2015\n                </text>\n                <line class=\"indicator\" [attr.x1]=\"pointCorX\" y1=\"110\" [attr.x2]=\"pointCorX\" y2=\"380\"></line>\n            </g>\n\n\n            <!-------------------------------------------------------- END INDICATOR  -------------------------------------------------------->\n\n            \n            <g class=\"labels x-labels\">\n                <text [attr.x]=\"item.value\" [attr.y]=\"400\" *ngFor=\"let item of items; let i=index; let last = last;\"\n                      (click)=\"getValue(i)\">\n                    {{item.name}}\n                </text>\n                <text x=\"105\" y=\"425\" font-size=\"16px\">2015</text>\n            </g>\n\n            <g class=\"labels y-labels\">\n                <text x=\"70\" y=\"120\">80</text>\n                <text x=\"70\" y=\"180\">60</text>\n                <text x=\"70\" y=\"240\">40</text>\n                <text x=\"70\" y=\"300\">20</text>\n                <text x=\"70\" y=\"365\">0</text>\n            </g>\n        </svg>\n\n    ",
             styleUrls: ['app/app.component.css']
         }), 
         __metadata('design:paramtypes', [])
--- app/app.component.ts
+++ app/app.component.ts
@@ -16,13 +16,12 @@ export class Item {
 @Component({
     selector: 'my-app',
     template: `
-        <svg class="graph" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"
-             (mousemove)="getPx($event.clientX)">
+        <svg class="graph" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" (mousemove)="getPx($event.clientX)">
+
             
-            <g class="indicator">
-                <line [attr.x1]="par" y1="110" [attr.x2]="par" y2="380"></line>
-            </g>
-            
+            <!--<g class="indicator" [ngStyle]="{'visibility': (indKey == true) ? 'visible' : 'hidden'}">-->
+
+            <!--<g class="grid y-grid" id="yGrid" (mousemove)="getW($event)">-->
             <g class="grid y-grid" id="yGrid">
                 <line x1="86" x2="95%" y1="126" y2="126"></line>
                 <line x1="86" x2="95%" y1="185" y2="185"></line>
@@ -31,22 +30,22 @@ export class Item {
                 <line x1="86" x2="95%" y1="360" y2="360"></line>
             </g>
 
-            
-            <polyline
-                    fill="none"
-                    stroke="#0074d9"
-                    stroke-width="2"
-                    points="
+            <!-------------------------------------------------------- GRAPH -------------------------------------------------------->
+            <polyline id="poly"
+                      fill="none"
+                      stroke="#0074d9"
+                      stroke-width="2"
+                      points="
                             110,130       
                             120,135      
-                            160, 140  
+                            160,140  
                             170,125   
                             175,160     
                             185,165  
                             195,175  
                             205,165 
-                            260, 160 
-                            290, 150 
+                            260,160 
+                            290,150 
                             315,180 
                             320,155
                             395,175 
@@ -73,9 +72,8 @@ export class Item {
                             910,250  
                             930,200  
                             945,185
-                       
                             960,205 
-                            980, 230  
+                            980,230  
                             1020,230   
                             1050,255  
                             1060,275 
@@ -86,7 +84,34 @@ export class Item {
                             1240,170
      "
             />
+            <!-------------------------------------------------------- END GRAPH -------------------------------------------------------->
 
+
+            
+            <!-------------------------------------------------------- INDICATOR  -------------------------------------------------------->
+            <circle class="shadow" [attr.cx]="pointCorX" [attr.cy]="pointCorY" r="5" stroke="#AAA" stroke-width="4"
+                    stroke-opacity="1"></circle>
+            
+            <g>
+                <filter id="dropShadow">
+                    <feGaussianBlur in="SourceAlpha" stdDeviation="3"/>
+                    <feOffset dx="2" dy="4"/>
+                    <feMerge>
+                        <feMergeNode/>
+                        <feMergeNode in="SourceGraphic"/>
+                    </feMerge>
+                </filter>
+                <rect [attr.x]="par" y="10" width="200" height="100" fill="white" filter="url(#dropShadow)"/>
+                <text [attr.x]="par+70" y="22%" alignment-baseline="middle" text-anchor="middle">
+                    {{pointCorX}}{{months[monthName - 1]}} 2015
+                </text>
+                <line class="indicator" [attr.x1]="pointCorX" y1="110" [attr.x2]="pointCorX" y2="380"></line>
+            </g>
+
+
+            <!-------------------------------------------------------- END INDICATOR  -------------------------------------------------------->
+
+            
             <g class="labels x-labels">
                 <text [attr.x]="item.value" [attr.y]="400" *ngFor="let item of items; let i=index; let last = last;"
                       (click)="getValue(i)">
@@ -110,6 +135,13 @@ export class Item {
 
 export class AppComponent {
     par = 70;
+    x = 0;
+    indKey = false;
+    monthName = 1;
+    pointCorX = 110;
+    d = document;
+    pointCorY = 130;
+    numDay = 1;
 
     items: Item[] =
         [
@@ -128,6 +160,8 @@ export class AppComponent {
 
         ];
 
+    months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+
 
     getValue(i) {
         var val = document.getElementsByTagName('text')[i].attributes['x'];
@@ -137,17 +171,44 @@ export class AppComponent {
     };
 
     getPx(event): void {
-        console.log(event);
         this.par = event;
+        this.monthName = Math.floor(this.par / 98);
+
+        var polyline = this.d.getElementById('poly'),
+            points = polyline.getAttribute('points'),
+            reg = /\d+/g,
+            coordinatAr = points.match(reg); //сформируем массив координат графика
+
+
+        for (var i = 0, max = coordinatAr.length; i < max; i++) {
+            if (i % 2 == 0) { //нам нужны координаты только по x
+                if (this.par == Number(coordinatAr[i])) {
+                    this.pointCorX = this.par;
+                    this.pointCorY = Number(coordinatAr[i + 1]);
+                    return;
+                }
+            }
+        }
+
 
     };
+
+    getW(event): void {
+        // event.currentTarget.getBBox().width;
+        var lineLength = event.currentTarget.getBBox().width; //длина линии
+        // var monthLength = Math.floor(lineLength / 12); //найдем сколько пикселей приходиться на каждый месяц (98px)
+        var monthLength = Math.floor(lineLength / 365); //найдем сколько пикселей приходиться на каждый день (3px)
+        console.log(monthLength);
+
+
+    };
+
 
     changedItem() {
         var count = 113;
         return function () {
             return count += 146;
         };
-
     };
 
     counter = this.changedItem();
